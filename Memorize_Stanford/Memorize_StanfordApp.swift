98360670cb0db9e1d527515a5985693df49ff8d5
evgeniasotnikova <sotnikova.zhenya@mal.ru>
2024-07-28 21:29:08 +0600

    Initial Commit

---
+++ Memorize_Stanford/Memorize_StanfordApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_StanfordApp.swift
+//  Memorize_Stanford
+//
+//  Created by Евгения  on 28.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_StanfordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
