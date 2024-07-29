898cd7a0fe7b99d6b0f1dcbe5297f261913dcb76
evgeniasotnikova <sotnikova.zhenya@mal.ru>
2024-07-29 17:41:55 +0600

    Togglabel isFaceUp

--- Memorize_Stanford/ContentView.swift
+++ Memorize_Stanford/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            CardView(isFaceUp: true)
+            CardView()
             CardView()
             CardView()
             CardView()
@@ -21,19 +21,21 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp: Bool = true
     var body: some View {
         ZStack {
+            let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("🧩").font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.pink)
+                base.fill()
             }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+            
         }
     }
 }
